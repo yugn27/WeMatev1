3fa0d40afac6b875534961102c78869929bcd0d5
Yash Nayak <yugn27@yashn-2.local>
2018-12-12 20:08:37 +0530

    Initial Commit

---
+++ WeMate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeMate
+//
+//  Created by Yash Nayak on 12/12/18.
+//  Copyright © 2018 Yash Nayak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
