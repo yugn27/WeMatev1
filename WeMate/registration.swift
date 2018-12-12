a8ab964e80fda6ea1f06eb3358d8cc2f8948e998
Vishitosh Kapale <phoenix@Vishitoshs-MacBook-Pro.local>
2018-12-13 00:49:40 +0530

    classes set for view controllers

---
+++ WeMate/registration.swift
@@ -0,0 +1,30 @@
+//
+//  registration.swift
+//  WeMate
+//
+//  Created by Vishitosh Vijay Kapale on 13/12/18.
+//  Copyright © 2018 Yash Nayak. All rights reserved.
+//
+
+import UIKit
+
+class registration: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
