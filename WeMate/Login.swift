7fad3d1ed1d7459a45394011796ba02d80c3379d
Yash Nayak <yugn27@yashn-2.local>
2018-12-22 11:49:04 +0530

    add group button and text field constrains set

--- WeMate/Login.swift
+++ WeMate/Login.swift
@@ -33,6 +33,8 @@ class Login: UIViewController {
                 self.myalert("Failed", "Incorrect Email ID or Password")
             }
         }
+        
+       
          self.clear()
     }
     override func viewDidLoad() {
