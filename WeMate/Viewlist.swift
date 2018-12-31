3a493ae0ec7f23b64a6af411f0cf06b82ffd01c8
Yash Nayak <yugn27@yashn-2.local>
2018-12-31 18:43:33 +0530

    31 Dec 2018 Backup

--- WeMate/Viewlist.swift
+++ WeMate/Viewlist.swift
@@ -7,15 +7,57 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
-class Viewlist: UIViewController {
+class Viewlist: UIViewController , UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var tableviewview: UITableView!
+    // Database  reference firebase
+    var ref : DatabaseReference!
+    var handle : DatabaseHandle!
+    
+    
+    var unicorns = [Unicorn]() {
+        didSet {
+            tableviewview.reloadData()
+        }
+    }
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableviewview.tableFooterView = UIView()
+        
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+        
+        // create record
+        ref = Database.database().reference()
+        
+        tableviewview.tableFooterView = UIView()
+        
         // Do any additional setup after loading the view.
     }
     
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        ref.child("unicorns").observe(.value) { snapshot in
+            var unicorns = [Unicorn]()
+            for unicornSnapshot in snapshot.children {
+                let unicorn = Unicorn(snapshot: unicornSnapshot as! DataSnapshot)
+                unicorns.append(unicorn)
+            }
+            self.unicorns = unicorns
+        }
+    }
 
     /*
     // MARK: - Navigation
@@ -26,5 +68,27 @@ class Viewlist: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
 
+}
+
+
+extension Viewlist {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return unicorns.count
+    }
+    
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "unicornTableViewCell", for: indexPath) as! UnicornTableViewCell
+        cell.unicorn = unicorns[indexPath.row]
+        return cell
+    }
 }
